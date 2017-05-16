05bcb88e9f10573de0d5030b50ff960da6fda402
AliakseiMiyusau <Aliaksei_Miyusau@epam.com>
2017-05-16 15:23:50 +0300

    Added face rectangle

--- face_landmarks_recognition.py
+++ face_landmarks_recognition.py
@@ -4,10 +4,12 @@ from google.cloud.vision.feature import Feature
 from google.cloud.vision.feature import FeatureTypes
 import cv2
 
-image1 = "/home/aliaksei/Documents/SemanticTube/test.jpg"
-image2 = "/home/aliaksei/Documents/SemanticTube/test2.jpg"
+image0 = "/home/aliaksei/Documents/SemanticTube/test0.jpg"
+image1 = "/home/aliaksei/Documents/SemanticTube/test1.jpg"
+image2 = "/home/aliaksei/Documents/SemanticTube/test2.png"
+image3 = "/home/aliaksei/Documents/SemanticTube/test3.jpg"
 
-image_list = [image1, image2]
+image_list = [image0, image1, image2, image3]
 
 def process_batch(image_list):
  
@@ -31,14 +33,19 @@ def main():
         img = cv2.imread(image_list[i])
         print image_list[i]
         for face in image.faces:
+
+            top_left_face_point = (face.bounds.vertices[0].x_coordinate, face.bounds.vertices[0].y_coordinate)
+            bot_right_face_point = (face.bounds.vertices[2].x_coordinate, face.bounds.vertices[2].y_coordinate)
+
             left_eye = (int(face.landmarks.left_eye.position.x_coordinate), int(face.landmarks.left_eye.position.y_coordinate))
             right_eye = (int(face.landmarks.right_eye.position.x_coordinate), int(face.landmarks.right_eye.position.y_coordinate))
             chin = (int(face.landmarks.chin_gnathion.position.x_coordinate), int(face.landmarks.chin_gnathion.position.y_coordinate))
-            left_eyebrow = (int(face.landmarks.left_eyebrow_upper_midpoint.position.x_coordinate), 
-                            int(face.landmarks.left_eyebrow_upper_midpoint.position.y_coordinate))
-            right_eyebrow = (int(face.landmarks.right_eyebrow_upper_midpoint.position.x_coordinate), 
-                             int(face.landmarks.right_eyebrow_upper_midpoint.position.y_coordinate))
+            left_eyebrow = (int(face.landmarks.left_eyebrow_upper_midpoint.position.x_coordinate), int(face.landmarks.left_eyebrow_upper_midpoint.position.y_coordinate))
+            right_eyebrow = (int(face.landmarks.right_eyebrow_upper_midpoint.position.x_coordinate), int(face.landmarks.right_eyebrow_upper_midpoint.position.y_coordinate))
             mouth = (int(face.landmarks.mouth_center.position.x_coordinate), int(face.landmarks.mouth_center.position.y_coordinate))
+
+            print("Top Left Face Point: " + str(top_left_face_point))
+            print("Bot Right Face Point: " + str(bot_right_face_point))
 
             print("Left Eye: " + str(left_eye))
             print("Right Eye: " + str(right_eye))
@@ -53,8 +60,10 @@ def main():
             cv2.circle(img, left_eyebrow, 3, (255, 255, 255))    
             cv2.circle(img, right_eyebrow, 3, (255, 255, 255))
             cv2.circle(img, mouth, 3, (255, 255, 255))
+            cv2.rectangle(img, top_left_face_point, bot_right_face_point, (255, 255, 0))
         cv2.imwrite("output" + str(i) + ".jpg", img)
         i+=1
+        
             
 if __name__ == "__main__":
     main()
